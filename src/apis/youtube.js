5ca3060942843266a3c41ed97916cf3526800787
Vahe <91395022+VaheHar@users.noreply.github.com>
2022-03-17 20:17:56 +0400

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //The key should be kept in back-end.There is no way to store it privately on front side.
-const KEY = "AIzaSyCnBNWkGKVe41BvyVtEO9kv12z-vxp2zx0";
+const KEY = process.env.API_KEY;
 
 export default axios.create({
 	baseURL: "https://www.googleapis.com/youtube/v3",
